a5cb985a9f8e458697e0aab9ff0f4f35363fbd58
SagredoIsmael <sagredo.ismael@live.com>
2020-05-29 17:07:33 +0200

    Updates

---
+++ precache-manifest.2176bcfab6ad898a9e916f3b088b7fa6.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7b55ffa14492a2e6118",
+    "url": "/citrikafm/static/js/app.184b8927.chunk.js"
+  },
+  {
+    "revision": "44a57b80592f5e6090df",
+    "url": "/citrikafm/static/js/runtime~app.79ca6687.js"
+  },
+  {
+    "revision": "96ef9294fbd69cdc235f",
+    "url": "/citrikafm/static/js/2.8f15c957.chunk.js"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/citrikafm/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/citrikafm/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/citrikafm/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/citrikafm/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/citrikafm/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/citrikafm/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/citrikafm/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/citrikafm/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/citrikafm/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/citrikafm/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/citrikafm/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/citrikafm/expo-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/citrikafm/serve.json"
+  },
+  {
+    "revision": "342cad6a7866f489131ad1c3405c9fa2",
+    "url": "/citrikafm/favicon-16.png"
+  },
+  {
+    "revision": "f2fd715865c19eda7d8c3fa76ab0a7e1",
+    "url": "/citrikafm/favicon-32.png"
+  },
+  {
+    "revision": "ecb8e6b726a7911fc1f01de407850e91",
+    "url": "/citrikafm/favicon.ico"
+  },
+  {
+    "revision": "52fdf306245e92c0a930bd98dde874ab",
+    "url": "/citrikafm/index.html"
+  },
+  {
+    "revision": "9bdc632f39ed9763c3647f37cf3a1161",
+    "url": "/citrikafm/manifest.json"
+  },
+  {
+    "revision": "42a9eac526b7dc6fc1cfad22df0e89b8",
+    "url": "/citrikafm/register-service-worker.js"
+  }
+];
