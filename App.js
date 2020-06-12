e54a630b9eebf1531fb02e0d86924a19fdd95da0
SagredoIsmael <sagredo.ismael@live.com>
2020-06-12 20:21:35 +0200

    add firebase packages && config

--- App.js
+++ App.js
@@ -6,13 +6,34 @@ import { store, persistor } from './src/redux/configureStore'
 import { PersistGate } from 'redux-persist/integration/react'
 import PlayerComponent from './src/redux/containers/player'
 
-export default () =>
-    <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-            <Navigator />
-            <PlayerComponent/>
-        </PersistGate>
-    </Provider>
+import * as firebase from 'firebase'
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDIZBtz_H3Y4C50eXoEPIE9d5kyz2kL0Hs",
+    authDomain: "citrikafm.firebaseapp.com",
+    databaseURL: "https://citrikafm.firebaseio.com",
+    projectId: "citrikafm",
+    storageBucket: "citrikafm.appspot.com",
+    messagingSenderId: "218124095941",
+    appId: "1:218124095941:web:62abbade64644dca2d3bac",
+    measurementId: "G-VLBND9SC4Q"
+}
+
+
+export default () => {
+    firebase.initializeApp(firebaseConfig)
+
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <Navigator />
+                <PlayerComponent />
+            </PersistGate>
+        </Provider>
+    )
+}
+
 
 
 // ignore specific yellowbox warnings
