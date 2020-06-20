f8878ced48604007d2e609e78602538e6d17fb26
SagredoIsmael <sagredo.ismael@live.com>
2020-06-20 12:35:56 +0200

    add logic for different dynamic list

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -2,11 +2,17 @@ import React, { useEffect } from 'react'
 import { StyleSheet, FlatList } from 'react-native'
 import Item from './Item'
 
-const List = ({ data, isWeb, isMain }) => 
+const List = ({ data, isWeb, isMain }) =>
   <FlatList
     style={styles.flatList}
     data={data}
-    renderItem={({ item }) => <Item item={item} isWeb={isWeb} />}
+    renderItem={({ item }) => {
+      if (item.only_in){
+        if (item.only_in == 'eventos' && isMain) return null
+        if (item.only_in == 'inicio' && !isMain) return null
+      }
+      return <Item item={item} isWeb={isWeb} />
+    }}
     keyExtractor={item => item.title}
   />
 
