4ac6d64c936ef35e84bd4e4bceed37d5ebe213fa
SagredoIsmael <sagredo.ismael@live.com>
2020-06-20 18:41:09 +0200

    add hovered package and implement hovered in menu items and icons

--- src/components/UI/Icon.js
+++ src/components/UI/Icon.js
@@ -2,20 +2,29 @@ import * as React from 'react'
 import { TouchableOpacity } from 'react-native'
 import { sizeNormalize } from '../../constants/layout'
 import Icon from 'react-native-vector-icons/Ionicons'
+import { Hoverable } from 'react-native-web-hooks'
 
-export default ({ onPress, name, size, color, style }) =>
+export default ({ onPress, name, size, color, style, backgroundColorHovered, colorHovered }) =>
     onPress ?
-        <TouchableOpacity 
-            style={style && style}
-            onPress={() => onPress()}>
-            {name && <Icon
-                name={name}
-                size={size ? sizeNormalize(size) : sizeNormalize(40)}
-                color={color ? color : 'black'} />}
-        </TouchableOpacity>
+        <Hoverable>
+            {isHovered => (
+                <TouchableOpacity accessible 
+                 style={{backgroundColor: isHovered && backgroundColorHovered, ...style}}
+                 onPress={() => onPress()}>
+                    {name &&
+                        <Icon
+                            name={name}
+                            size={size ? sizeNormalize(size) : sizeNormalize(40)}
+                            color={isHovered? colorHovered : color} />
+                    }
+                </TouchableOpacity>
+            )}
+        </Hoverable>
         :
         name && <Icon
             name={name}
             size={size ? sizeNormalize(size) : sizeNormalize(40)}
             color={color ? color : 'black'} />
+
+
 
--- src/components/drawer/Drawer.js
+++ src/components/drawer/Drawer.js
@@ -9,6 +9,7 @@ import { screens } from '../../constants/navigation'
 import constants from '../../constants/fetch'
 import AntDesign from 'react-native-vector-icons/AntDesign'
 import Feather from 'react-native-vector-icons/Feather'
+import { Hoverable } from 'react-native-web-hooks'
 
 
 const { MAIN, EVENTS, DEEJAYS, CONTACT, LEGAL } = screens
@@ -16,53 +17,57 @@ const { MAIN, EVENTS, DEEJAYS, CONTACT, LEGAL } = screens
 const drawerItems = [
     {
         label: MAIN,
-        icon: () => <AntDesign color={colors.white} size={sizeNormalize(22)} name={'home'}/>
+        icon: () => <AntDesign color={colors.white} size={sizeNormalize(22)} name={'home'} />
     },
     {
         label: EVENTS,
-        icon: () => <Feather color={colors.white} size={sizeNormalize(22)} name={'calendar'}/>
+        icon: () => <Feather color={colors.white} size={sizeNormalize(22)} name={'calendar'} />
     },
     {
         label: DEEJAYS,
-        icon: () => <Feather color={colors.white} size={sizeNormalize(22)} name={'sliders'}/>
+        icon: () => <Feather color={colors.white} size={sizeNormalize(22)} name={'sliders'} />
     },
     {
         label: CONTACT,
-        icon: () => <AntDesign color={colors.white} size={sizeNormalize(22)} name={'mail'}/>
+        icon: () => <AntDesign color={colors.white} size={sizeNormalize(22)} name={'mail'} />
     },
     {
         label: LEGAL,
-        icon: () => <Feather color={colors.white} size={sizeNormalize(22)} name={'file-text'}/>
+        icon: () => <Feather color={colors.white} size={sizeNormalize(22)} name={'file-text'} />
     }
 ]
 
 export default ({ navigation }) =>
-    <View style={styles.drawerContainer}>
+    <View style={styles().drawerContainer}>
         <View>
             {map(drawerItems, drawerItem =>
-                <DrawerItem 
-                    key={drawerItem.label}
-                    onPress={() => navigation.navigate(drawerItem.label)}
-                    labelStyle={styles.labelDrawer}
-                    style={styles.drawerItem}
-                    {...drawerItem} />
+                <Hoverable>
+                    {isHovered => (
+                        <DrawerItem
+                        key={drawerItem.label}
+                        onPress={() => navigation.navigate(drawerItem.label)}
+                        labelStyle={styles().labelDrawer}
+                        style={styles(isHovered).drawerItem}
+                        {...drawerItem} />
+                    )}
+                </Hoverable>
             )}
         </View>
-        <View style={styles.socialDrawerItem}>
-            <Icon onPress={() => window.open(constants.INSTAGRAM,'_blank')} name={'logo-instagram'} color={colors.white} size={30} />
-            <Icon onPress={() => window.open(constants.FACEBOOK,'_blank')} name={'logo-facebook'} color={colors.white} size={30} />
-            <Icon onPress={() => window.open(constants.TWITTER,'_blank')} name={'logo-twitter'} color={colors.white} size={30} />
+        <View style={styles().socialDrawerItem}>
+            <Icon onPress={() => window.open(constants.INSTAGRAM, '_blank')} name={'logo-instagram'} color={colors.white} colorHovered={colors.primary} size={30} />
+            <Icon onPress={() => window.open(constants.FACEBOOK, '_blank')} name={'logo-facebook'} color={colors.white} colorHovered={colors.primary} size={30} />
+            <Icon onPress={() => window.open(constants.TWITTER, '_blank')} name={'logo-twitter'} color={colors.white} colorHovered={colors.primary} size={30} />
         </View>
     </View>
 
 
-const styles = StyleSheet.create({
+const styles = (isHovered) => StyleSheet.create({
     drawerContainer: {
         flex: 1,
         backgroundColor: colors.black,
         justifyContent: 'space-between',
         padding: '7%',
-        paddingTop:'20%',
+        paddingTop: '20%',
     },
     labelDrawer: {
         color: colors.white,
@@ -71,11 +76,12 @@ const styles = StyleSheet.create({
     drawerItem: {
         borderBottomColor: colors.primary,
         borderBottomWidth: 1,
-        marginTop:'5%',
+        marginTop: '5%',
+        backgroundColor: isHovered? colors.primary : colors.black
     },
     socialDrawerItem: {
-        marginLeft:'5%',
-        marginRight:'5%',
+        marginLeft: '5%',
+        marginRight: '5%',
         flexDirection: 'row',
         justifyContent: 'space-between'
     }
