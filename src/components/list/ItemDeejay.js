7311de9e1b480299a46c11ddcba80f7c9686fd2f
SagredoIsmael <sagredo.ismael@live.com>
2020-06-26 15:52:03 +0200

    update styles && add window open blank

--- src/components/list/ItemDeejay.js
+++ src/components/list/ItemDeejay.js
@@ -3,10 +3,8 @@ import { StyleSheet, Image, Text, View, TouchableOpacity, Linking } from 'react-
 import { sizeNormalize } from '../../constants/layout'
 import colors from '../../constants/colors'
 import Icon from '../UI/Icon'
-import constants from '../../constants/fetch'
 
-
-export default ({ item, isWeb }) =>
+export default ({ item, isWeb, isMobile }) =>
     <View
         style={styles(isWeb).container}>
         <Image
@@ -14,15 +12,16 @@ export default ({ item, isWeb }) =>
             source={{ uri: item.photo }} />
         <View style={styles(isWeb).infoWrapper}>
             <TouchableOpacity
-                onPress={() => item.web && Linking.openURL(item.web)}>
+                style={styles(isWeb).textWrapper}
+                onPress={() => item.web && isMobile? Linking.openURL(item.web) : window.open(item.web)}>
                 <Text style={styles(isWeb).textTitle}>
                     {item.name}
                 </Text>
             </TouchableOpacity>
             <View style={styles(isWeb).socialDrawerItem}>
-                {item.instagram && <Icon onPress={() => Linking.openURL(item.instagram)} name={'logo-instagram'} color={colors.white} colorHovered={colors.primary} size={30} />}
-                {item.facebook && <Icon onPress={() => Linking.openURL(item.facebook)} name={'logo-facebook'} color={colors.white} colorHovered={colors.primary} size={30} />}
-                {item.twitter && <Icon onPress={() => Linking.openURL(item.twitter)} name={'logo-twitter'} color={colors.white} colorHovered={colors.primary} size={30} />}
+                {item.facebook && <Icon onPress={() => isMobile? Linking.openURL(item.facebook) : window.open(item.facebook)} name={'logo-facebook'} color={colors.white} colorHovered={colors.primary} size={30}  />}
+                {item.instagram && <Icon onPress={() => isMobile? Linking.openURL(item.instagram) : window.open(item.instagram)} name={'logo-instagram'} color={colors.white} colorHovered={colors.primary} size={30} style={{marginLeft: '10%'}}/>}
+                {item.twitter && <Icon onPress={() => isMobile? Linking.openURL(item.twitter) : window.open(item.twitter)} name={'logo-twitter'} color={colors.white} colorHovered={colors.primary} size={30} style={{marginLeft: '10%'}}/>}
             </View>
         </View>
     </View>
@@ -56,16 +55,21 @@ const styles = (isWeb) => StyleSheet.create({
         flexDirection: 'row',
         marginBottom: isWeb ? '30%' : '20%',
         width: '30%',
-        justifyContent: 'space-between'
+        justifyContent: 'center',
+        alignItems: 'center'
+
+    },
+    textWrapper:{
+        flex:1,
+        borderBottomWidth: 1,
+        borderColor: colors.lightBlack,
+        margin: '5%',
+        marginTop: isWeb ? '30%' : '20%',
     },
     textTitle: {
         color: colors.white,
         fontSize: isWeb ? sizeNormalize(40) : sizeNormalize(20),
         fontWeight: "bold",
         textAlign: 'center',
-        borderBottomWidth: 1,
-        borderColor: colors.lightBlack,
-        margin: '5%',
-        marginTop: isWeb ? '30%' : '20%',
     },
 })
