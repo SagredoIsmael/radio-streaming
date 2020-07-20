63394a2aa228f644a036d07cb0988548ad547acf
SagredoIsmael <sagredo.ismael@live.com>
2020-07-20 19:07:28 +0200

    add dummy file to ios for track

--- dummy.swift
+++ dummy.swift
@@ -2,7 +2,7 @@
 //  dummy.swift
 //  CitrikaFm
 //
-//  Created by Ismael Sagredo on 17/07/2020.
+//  Created by Ismael Sagredo on 20/07/2020.
 //  Copyright © 2020 Facebook. All rights reserved.
 //
 
